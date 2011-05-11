e00f424d025ae6e29ca3651f7a11ed62de293758
sunnavy <sunnavy@gmail.com>
2011-05-11 21:41:57 +0800

    test server

--- lib/Beagle/Test.pm
+++ lib/Beagle/Test.pm
@@ -5,13 +5,39 @@ use base 'Exporter';
 use Beagle::Util;
 use File::Temp 'tempdir';
 use Test::More;
-our @EXPORT = qw/create_tmp_beagle/;
+our @EXPORT = qw/create_tmp_beagle start_server/;
 
 sub create_tmp_beagle {
     my $root = tempdir( CLEANUP => 1 );
     local $Test::Builder::Level = $Test::Builder::Level + 1;
     ok( create_beagle( type => 'fs', root => $root, @_ ), "created beagle $root" );
     return $root;
+}
+
+my @pids;
+
+sub start_server {
+    my %args = @_;
+    local $ENV{BEAGLE_ROOT} = create_tmp_beagle( type => 'git', @_ );
+
+    my $pid = fork();
+    die "failed to fork" unless defined $pid;
+
+    my $port = 5000 + int rand 10_000;
+
+    if ($pid) {
+        push @pids, $pid;
+        sleep 1;
+        return ("http://localhost:$port", $ENV{BEAGLE_ROOT});
+    }
+    else {
+        exec qw/beagle web -E deployment --port/, $port, @_;
+        exit;
+    }
+}
+
+END {
+    kill 'TERM', @pids;
 }
 
 1;
