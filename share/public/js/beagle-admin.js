40d24b84434ecf4419176cd67df1c4d978196816
sunnavy <sunnavy@gmail.com>
2011-05-15 18:59:57 +0800

    seperate admin js and rewrite search ajax so we can avoid jqueyr.form.js in non-admin mode

---
+++ share/public/js/beagle-admin.js
@@ -0,0 +1,143 @@
+/*
+ * Beagle JavaScript Library
+ *
+ * Copyright 2011, sunnavy@gmail.com
+ * licensed under the GPL Version 2.
+ */
+
+function beagleBindKeys () {
+    $('textarea').keydown(function (e) {
+        if ( e.keyCode == 13 && e.ctrlKey ) {
+            $(this).closest('form').submit();
+            $(this).val('');
+        }
+    } );
+}
+
+function beagleAjaxDelete () {
+    $('form.delete').ajaxForm( {
+        url: '/admin/entry/delete',
+        dataType: 'json',
+        type: 'post',
+        beforeSubmit: function (arr, form) {
+            var yes = confirm('really delete?');
+            if ( !yes ) {
+                return false;
+            }
+            var id = form.find('input[name=id]').val();
+            if ( !id ) {
+                return false;
+            }
+            return true;
+        },
+        success: function( json, status, xhr, form ) {
+            if ( json && json.status == 'deleted' ) {
+                var id = form.find('input[name=id]').val();
+                $('div#'+id).remove();
+                if ( json.redraw_menu ) {
+                    $('#menu').load('/fragment/menu', function () {
+                        beagleContrast('#menu');
+                    } );
+                }
+                beagleContrast('#content');
+            }
+        },
+    } );
+}
+
+function beagleAjaxComment ( admin ) {
+    $('div.create-comment form').ajaxForm(
+            {
+                beforeSubmit: function (arr,form) {
+                    var e = form.find('textarea');
+                    if ( beagleIsEmpty( e ) ) {
+                        return false;
+                    }
+                    else if ( !beagleIsInRange( e, 0, 1000 ) ) {
+                        $('<div></div>').html('Content is too large!').dialog({
+                            autoOpen: true,
+                            title: 'Error!',
+                            show: 'slide',
+                        });
+                        return false;
+                    }
+
+                    else {
+                        return true;
+                    }
+                },
+                url: "/admin/entry/comment/new",
+                dataType: 'json',
+                type: 'post',
+                success: function(json, status, xhr, form ) {
+                    form.submitted = false;
+                    if ( json ) {
+
+                        if ( json.status == 'created' ) {
+                            var str = json.content;
+                            form.find('textarea').val('');
+                            if ( admin ) {
+                                var parent = form.closest('div.comments').children('div.content');
+                                parent.append(str);
+                                beagleContrast(parent);
+                            }
+                            else {
+                                $('<div></div>').html('Thanks for your comment!').dialog({
+                                    autoOpen: true,
+                                    title: 'Sent!',
+                                    show: 'slide',
+                                });
+                            }
+                            return true;
+                        }
+                        else {
+                            $('<div></div>').html(json.message).dialog({
+                                autoOpen: true,
+                                title: json.status,
+                                show: 'slide',
+                            });
+                        }
+                    }
+                },
+            }
+    );
+}
+
+function beagleAdminInit ( ) {
+
+    $('select[name=format]').change( function() {
+        var val = $('select[name=format]').val();
+        var e = $(this).closest('form').find('textarea')
+        var form = $(this).closest('form');
+        if ( val == 'plain' ) {
+            e.markItUpRemove();
+        }
+        else if ( val == 'wiki' ) {
+            if ( !form.find('div.markItUp').length ) {
+                e.markItUp( wikiSettings );
+            }
+        }
+        else if ( val == 'markdown' ) {
+            if ( !form.find('div.markItUp').length ) {
+                e.markItUp( markdownSettings );
+            }
+        }
+    });
+
+
+    $('a.delete').click( function() {
+        var form = $(this).closest('form');
+        if ( form ) {
+            form.submit();
+        }
+        return false;
+    } );
+
+    beagleAjaxComment( 1 );
+    beagleAjaxDelete();
+    beagleBindKeys();
+
+    $('textarea.markitup.wiki').markItUp( wikiSettings );
+    $('textarea.markitup.markdown').markItUp( markdownSettings );
+}
+
--- share/public/js/beagle.js
+++ share/public/js/beagle.js
@@ -2,7 +2,7 @@
  * Beagle JavaScript Library
  *
  * Copyright 2011, sunnavy@gmail.com
- * Dual licensed under the MIT or GPL Version 2 licenses.
+ * licensed under GPL Version 2.
  */
 
 function beagleContrast (p) {
@@ -42,35 +42,17 @@ function beagleToggle ( ) {
     }
 }
 
-function beagleBindKeys () {
-    $('textarea').keydown(function (e) {
-        if ( e.keyCode == 13 && e.ctrlKey ) {
-            $(this).closest('form').submit();
-            $(this).val('');
-        }
-    } );
-}
-
 function beagleAjaxSearch () {
-    $('form.search').ajaxForm( {
-        url: '/search',
-        dataType: 'json',
-        type: 'post',
-        beforeSubmit: function (arr, form) {
-            var e = form.find('input[name=query]');
-            if ( beagleIsEmpty( e ) ) {
-                return false;
-            }
-            else {
-                return true;
-            }
-        },
-        success: function( json, status, xhr, form ) {
-            form.submitted = false;
-
-            if ( json ) {
-                if ( json.results.length >= 1 ) {
-                    form.find('input').val('');
+    $('#search form').submit( function () {
+        var form = $(this);
+        var query = form.find('input[name=query]');
+        if ( ! beagleIsEmpty( query ) ) {
+            $.ajax( {
+                type: 'post',
+                url: '/search',
+                data: { query: query.val() },
+                dataType: 'json',
+                success: function (json) {
                     if ( json.results.length == 1 ) {
                         $('#content').load(
                             '/fragment/entry/' + json.results[0].id, function() {
@@ -87,102 +69,12 @@ function beagleAjaxSearch () {
                         html += '</ul></div>';
                         $('#content').html( html );
                     }
-                }
-                else {
-                    $('#content').html( '<div class="warnings">not found</div>' );
-                }
-            }
-        },
-    } );
-}
+                    $('#search form').find('input[name=query]').val('');
 
-function beagleAjaxDelete () {
-    $('form.delete').ajaxForm( {
-        url: '/admin/entry/delete',
-        dataType: 'json',
-        type: 'post',
-        beforeSubmit: function (arr, form) {
-            var yes = confirm('really delete?');
-            if ( !yes ) {
-                return false;
-            }
-            var id = form.find('input[name=id]').val();
-            if ( !id ) {
-                return false;
-            }
-            return true;
-        },
-        success: function( json, status, xhr, form ) {
-            if ( json && json.status == 'deleted' ) {
-                var id = form.find('input[name=id]').val();
-                $('div#'+id).remove();
-                if ( json.redraw_menu ) {
-                    $('#menu').load('/fragment/menu', function () {
-                        beagleContrast('#menu');
-                    } );
-                }
-                beagleContrast('#content');
-            }
-        },
-    } );
-}
-
-function beagleAjaxComment ( admin ) {
-    $('div.create-comment form').ajaxForm(
-            {
-                beforeSubmit: function (arr,form) {
-                    var e = form.find('textarea');
-                    if ( beagleIsEmpty( e ) ) {
-                        return false;
-                    }
-                    else if ( !beagleIsInRange( e, 0, 1000 ) ) {
-                        $('<div></div>').html('Content is too large!').dialog({
-                            autoOpen: true,
-                            title: 'Error!',
-                            show: 'slide',
-                        });
-                        return false;
-                    }
-
-                    else {
-                        return true;
-                    }
-                },
-                url: "/admin/entry/comment/new",
-                dataType: 'json',
-                type: 'post',
-                success: function(json, status, xhr, form ) {
-                    form.submitted = false;
-                    if ( json ) {
-
-                        if ( json.status == 'created' ) {
-                            var str = json.content;
-                            form.find('textarea').val('');
-                            if ( admin ) {
-                                var parent = form.closest('div.comments').children('div.content');
-                                parent.append(str);
-                                beagleContrast(parent);
-                            }
-                            else {
-                                $('<div></div>').html('Thanks for your comment!').dialog({
-                                    autoOpen: true,
-                                    title: 'Sent!',
-                                    show: 'slide',
-                                });
-                            }
-                            return true;
-                        }
-                        else {
-                            $('<div></div>').html(json.message).dialog({
-                                autoOpen: true,
-                                title: json.status,
-                                show: 'slide',
-                            });
-                        }
-                    }
-                },
-            }
-    );
+                } } );
+        }
+        return false;
+    } )
 }
 
 function beagleArchive ( ) {
@@ -225,52 +117,11 @@ function beagleInit ( opts ) {
         }
     );
 
-    $('select[name=format]').change( function() {
-        var val = $('select[name=format]').val();
-        var e = $(this).closest('form').find('textarea')
-        var form = $(this).closest('form');
-        if ( val == 'plain' ) {
-            e.markItUpRemove();
-        }
-        else if ( val == 'wiki' ) {
-            if ( !form.find('div.markItUp').length ) {
-                e.markItUp( wikiSettings );
-            }
-        }
-        else if ( val == 'markdown' ) {
-            if ( !form.find('div.markItUp').length ) {
-                e.markItUp( markdownSettings );
-            }
-        }
-    });
-
-
-    $('a.delete').click( function() {
-        var form = $(this).closest('form');
-        if ( form ) {
-            form.submit();
-        }
-        return false;
-    } );
-
-    beagleAjaxComment( opts['admin'] );
-
-    if ( opts['admin'] ) {
-        beagleAjaxDelete();
-    }
-
     beagleAjaxSearch();
     beagleArchive();
-    beagleBindKeys();
     beagleContrast();
 
     $('div.message').delay(3000).fadeOut('slow');
-
-    if ( opts['admin'] ) {
-        $('textarea.markitup.wiki').markItUp( wikiSettings );
-        $('textarea.markitup.markdown').markItUp( markdownSettings );
-    }
-
 
     $('form').submit(function() {
         if (this.submitted) {
