fea2049e8c6d546204eb49162081129a7bc5e3e3
sunnavy <sunnavy@gmail.com>
2011-05-26 23:45:04 +0800

    switch id and cmd for spread

--- lib/Beagle/Cmd/Command/spread.pm
+++ lib/Beagle/Cmd/Command/spread.pm
@@ -4,10 +4,10 @@ use Beagle::Util;
 use Encode;
 extends qw/Beagle::Cmd::Command/;
 
-has 'id' => (
+has 'cmd' => (
     isa           => 'Str',
     is            => 'rw',
-    documentation => 'entry id',
+    documentation => 'spread cmd',
     traits        => ['Getopt'],
 );
 
@@ -38,46 +38,47 @@ __PACKAGE__->meta->make_immutable;
 
 sub execute {
     my ( $self, $opt, $args ) = @_;
-    die "beagle spread --id id target [...]" unless @$args && $self->id;
+    die "beagle spread --cmd spread-cmd id1 id2 [...]"
+      unless @$args && $self->cmd;
 
-    my $i = $self->id;
-    my @ret = resolve_id( $i, handler => handler() || undef );
-    unless (@ret) {
-        @ret = resolve_id($i) or die_id_invalid($i);
-    }
-    die_id_ambiguous( $i, @ret ) unless @ret == 1;
-    my $id    = $ret[0]->{id};
-    my $bh    = $ret[0]->{handler};
-    my $entry = $ret[0]->{entry};
-
-    my $msg;
-    if ( $entry->type eq 'bark' ) {
-        $msg = $entry->summary(70);
-    }
-    else {
-        my $url = $bh->info->url . '/entry/' . $entry->id;
-        if ( $self->shorten ) {
-            my $s = `shorten $url`;
-            chomp $s;
-            die "shorten failed" unless $s && $s =~ /\S/;
-            $url = $s;
+    my $cmd = $self->cmd;
+    for my $i (@$args) {
+        my @ret = resolve_id( $i, handler => handler() || undef );
+        unless (@ret) {
+            @ret = resolve_id($i) or die_id_invalid($i);
         }
-        $msg = join ' ', $entry->title, $url;
-    }
-    if ( $self->dry_run ) {
-        puts "going to call `$_ $msg`" for @$args;
-    }
-    else {
-        my $update = 1;
-        if ( !$self->quiet ) {
-            puts "going to call `$_ $msg`" for @$args;
-            print "spread? (Y/n): ";
-            my $val = <STDIN>;
-            undef $update if $val =~ /n/i;
-        }
+        die_id_ambiguous( $i, @ret ) unless @ret == 1;
+        my $id    = $ret[0]->{id};
+        my $bh    = $ret[0]->{handler};
+        my $entry = $ret[0]->{entry};
 
-        if ($update) {
-            for my $cmd (@$args) {
+        my $msg;
+        if ( $entry->type eq 'bark' ) {
+            $msg = $entry->summary(70);
+        }
+        else {
+            my $url = $bh->info->url . '/entry/' . $entry->id;
+            if ( $self->shorten ) {
+                my $s = `shorten $url`;
+                chomp $s;
+                die "shorten failed" unless $s && $s =~ /\S/;
+                $url = $s;
+            }
+            $msg = join ' ', $entry->title, $url;
+        }
+        if ( $self->dry_run ) {
+            puts "going to call `$cmd $msg`";
+        }
+        else {
+            my $update = 1;
+            if ( !$self->quiet ) {
+                puts "going to call `$cmd $msg`";
+                print "spread? (Y/n): ";
+                my $val = <STDIN>;
+                undef $update if $val =~ /n/i;
+            }
+
+            if ($update) {
                 my @cmd = Text::ParseWords::shellwords($cmd);
                 system( @cmd, $msg ) == 0 or die $!;
             }
