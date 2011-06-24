a26e90b211fd52ef844dae6b02130f6e7cf55dd2
sunnavy <sunnavy@gmail.com>
2011-06-24 15:44:51 +0800

    allow to update multiple entries at one

--- lib/Beagle/Cmd/Command/update.pm
+++ lib/Beagle/Cmd/Command/update.pm
@@ -40,81 +40,85 @@ __PACKAGE__->meta->make_immutable;
 
 sub execute {
     my ( $self, $opt, $args ) = @_;
-    die "beagle update id" unless @$args && @$args == 1;
+    die "beagle update id [...]" unless @$args;
 
-    my $i = $args->[0];
-    my @ret = resolve_entry( $i, handler => handler() || undef );
-    unless (@ret) {
-        @ret = resolve_entry($i) or die_entry_not_found($i);
-    }
-    die_entry_ambiguous( $i, @ret ) unless @ret == 1;
-    my $id    = $ret[0]->{id};
-    my $bh    = $ret[0]->{handler};
-    my $entry = $ret[0]->{entry};
+    for my $i (@$args) {
+        my @ret = resolve_entry( $i, handler => handler() || undef );
+        unless (@ret) {
+            @ret = resolve_entry($i) or die_entry_not_found($i);
+        }
+        die_entry_ambiguous( $i, @ret ) unless @ret == 1;
+        my $id    = $ret[0]->{id};
+        my $bh    = $ret[0]->{handler};
+        my $entry = $ret[0]->{entry};
 
-    if ( $self->set ) {
-        for my $item ( @{ $self->set } ) {
-            my ( $key, $value ) = split /=/, $item, 2;
-            if ( $entry->can($key) ) {
-                $entry->$key($value);
-            }
-            else {
-                warn "unknown key: $key\n";
+        if ( $self->set ) {
+            for my $item ( @{ $self->set } ) {
+                my ( $key, $value ) = split /=/, $item, 2;
+                if ( $entry->can($key) ) {
+                    $entry->$key($value);
+                }
+                else {
+                    warn "unknown key: $key\n";
+                }
             }
         }
-    }
 
-    if ( $self->edit || !$self->set ) {
-        my $template = encode_utf8 $entry->serialize(
-            $self->verbose
-            ? (
-                path      => 1,
-                created   => 1,
-                updated   => 1,
-                id        => 1,
-                parent_id => 1,
-              )
-            : (
-                path      => undef,
-                created   => undef,
-                updated   => undef,
-                id        => undef,
-                parent_id => undef,
+        if ( $self->edit || !$self->set ) {
+            my $template = encode_utf8 $entry->serialize(
+                $self->verbose
+                ? (
+                    path      => 1,
+                    created   => 1,
+                    updated   => 1,
+                    id        => 1,
+                    parent_id => 1,
+                  )
+                : (
+                    path      => undef,
+                    created   => undef,
+                    updated   => undef,
+                    id        => undef,
+                    parent_id => undef,
+                )
+            );
+
+            my $updated = edit_text($template);
+
+            if ( !$self->force && $template eq $updated ) {
+                puts "aborted.";
+                return;
+            }
+            my $updated_entry =
+              $entry->new_from_string( decode_utf8($updated),
+                $self->verbose ? () : ( id => $entry->id ) );
+            $updated_entry->original_path( $entry->original_path );
+
+            unless ( $self->verbose ) {
+                if ( $entry->can('parent_id') ) {
+                    $updated_entry->parent_id( $entry->parent_id );
+                }
+
+                $updated_entry->created( $entry->created );
+                $updated_entry->updated(time);
+            }
+
+            $updated_entry->timezone( $bh->info->timezone )
+              if $bh->info->timezone;
+            $entry = $updated_entry;
+        }
+
+        if (
+            $bh->update_entry(
+                $entry, message => $self->message || "updated $id"
             )
-        );
-
-        my $updated = edit_text($template);
-
-        if ( !$self->force && $template eq $updated ) {
-            puts "aborted.";
-            return;
+          )
+        {
+            puts 'updated ', $entry->id, ".";
         }
-        my $updated_entry =
-          $entry->new_from_string( decode_utf8($updated),
-            $self->verbose ? () : ( id => $entry->id ) );
-        $updated_entry->original_path( $entry->original_path );
-
-        unless ( $self->verbose ) {
-            if ( $entry->can('parent_id') ) {
-                $updated_entry->parent_id( $entry->parent_id );
-            }
-
-            $updated_entry->created( $entry->created );
-            $updated_entry->updated(time);
+        else {
+            die "failed to update " . $entry->id . '.';
         }
-
-        $updated_entry->timezone( $bh->info->timezone )
-          if $bh->info->timezone;
-        $entry = $updated_entry;
-    }
-
-    if ( $bh->update_entry( $entry, message => $self->message || "updated $id" )
-      )
-    {
-        puts 'updated ', $entry->id, ".";
-    }
-    else {
-        die "failed to update " . $entry->id . '.';
     }
 }
 
