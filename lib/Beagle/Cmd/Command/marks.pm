73c478516bebabfb92f29540945f8098f07cde33
sunnavy <sunnavy@gmail.com>
2011-06-25 16:35:07 +0800

    support marks export and import

--- lib/Beagle/Cmd/Command/marks.pm
+++ lib/Beagle/Cmd/Command/marks.pm
@@ -8,37 +8,85 @@ __PACKAGE__->meta->make_immutable;
 
 sub execute {
     my ( $self, $opt, $args ) = @_;
-    my $updated;
+
     my $marks = entry_marks;
 
-    my @ids;
-    if (@$args) {
-        for my $i (@$args) {
-            my @ret = resolve_entry( $i, handler => handler() || undef );
-            unless (@ret) {
-                @ret = resolve_entry($i) or die_entry_not_found($i);
+    my $subcmd;
+    if ( @$args && $args->[0] =~ /^(?:export|import)$/ ) {
+        $subcmd = shift @$args;
+    }
+
+    if ($subcmd) {
+        require JSON;
+        if ( $subcmd eq 'export' ) {
+            my $path = shift @$args;
+            my $converted = {};
+            for my $id ( keys %$marks ) {
+                if ( $marks->{$id} && %{ $marks->{$id} } ) {
+                    $converted->{$id} = [ sort keys %{ $marks->{$id} } ];
+                }
             }
-            die_entry_ambiguous( $i, @ret ) unless @ret == 1;
-            my $id = $ret[0]->{id};
-            push @ids, $id;
+
+            if ( $path && $path ne '-' ) {
+                my $out = JSON::to_json( $converted, { utf8 => 1, pretty => 1 } );
+                write_file( $path, $out );
+                puts 'exported.';
+            }
+            else {
+                my $out = JSON::to_json( $converted, { pretty => 1 } );
+                puts $out;
+            }
+        }
+        elsif ( $subcmd eq 'import' ) {
+            my $path = shift @$args;
+            my $in;
+            if ( $path && $path ne '-' ) {
+                $in = decode( 'utf8', read_file( $path ) );
+            }
+            else {
+                local $/;
+                $in = decode( locale => <STDIN> );
+            }
+            my $converted = JSON::from_json( $in );
+            for my $id ( keys %$marks ) {
+                next unless $converted->{$id} && @{ $converted->{$id} };
+                $marks->{$id} = { map { $_ => 1 } @{ $converted->{$id} } };
+            }
+            set_entry_marks($marks);
+            puts 'imported.';
         }
     }
     else {
-        @ids = keys %$marks;
-    }
 
-    @ids = grep { %{ $marks->{$_} } } @ids;
+        my @ids;
+        if (@$args) {
+            for my $i (@$args) {
+                my @ret = resolve_entry( $i, handler => handler() || undef );
+                unless (@ret) {
+                    @ret = resolve_entry($i) or die_entry_not_found($i);
+                }
+                die_entry_ambiguous( $i, @ret ) unless @ret == 1;
+                my $id = $ret[0]->{id};
+                puts @ids, $id;
+            }
+        }
+        else {
+            @ids = keys %$marks;
+        }
 
-    if (@ids) {
-        require Text::Table;
-        my $tb = Text::Table->new();
-        $tb->load( map { [ $_, join ', ', sort keys %{ $marks->{$_} } ] }
-              @ids );
-        puts $tb;
+        @ids = grep { %{ $marks->{$_} } } @ids;
+
+        if (@ids) {
+            require Text::Table;
+            my $tb = Text::Table->new();
+            $tb->load( map { [ $_, join ', ', sort keys %{ $marks->{$_} } ] }
+                  @ids );
+            puts $tb;
+        }
     }
 }
 
-sub usage_desc { 'show entry marks' }
+sub usage_desc { 'show/export/import entry marks' }
 
 1;
 
@@ -46,7 +94,7 @@ __END__
 
 =head1 NAME
 
-Beagle::Cmd::Command::marks - show entry marks
+Beagle::Cmd::Command::marks - show/export/import entry marks
 
 =head1 AUTHOR
 
