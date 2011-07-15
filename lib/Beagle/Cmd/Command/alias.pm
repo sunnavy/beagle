2004ec1d5971df833ee557b2c14bbcf27a366384
sunnavy <sunnavy@gmail.com>
2011-07-15 12:27:19 +0800

    tweak and document alias

--- lib/Beagle/Cmd/Command/alias.pm
+++ lib/Beagle/Cmd/Command/alias.pm
@@ -5,14 +5,14 @@ use Any::Moose;
 extends qw/Beagle::Cmd::GlobalCommand/;
 
 has set => (
-    isa           => "Bool",
-    is            => "rw",
-    documentation => "set",
+    isa           => 'ArrayRef[Str]',
+    is            => 'rw',
+    documentation => 'set',
     traits        => ['Getopt'],
 );
 
 has unset => (
-    isa           => "Bool",
+    isa           => 'ArrayRef[Str]',
     is            => "rw",
     documentation => "unset",
     traits        => ['Getopt'],
@@ -32,26 +32,44 @@ sub execute {
 
     if ( $self->set || $self->unset ) {
 
+        my @added;
+        my @removed;
         if ( $self->set ) {
-            for my $item (@$args) {
+            for my $item (@{$self->set}) {
                 my ( $name, $value ) = split /=/, $item, 2;
                 $user_alias->{$name} = $value;
+                push @added, $name;
             }
         }
-        elsif ( $self->unset ) {
-            for my $name (@$args) {
+
+        if ( $self->unset ) {
+            for my $name (@{$self->unset}) {
                 delete $user_alias->{$name};
+                push @removed, $name;
             }
         }
         set_user_alias($user_alias);
-        puts "updated.";
+
+        if ( @added ) {
+            puts 'added ', join ', ', @added;
+        }
+        if ( @removed ) {
+            puts 'removed ', join ', ', @removed;
+        }
+
         return;
     }
 
     if (@$args) {
         for my $key (@$args) {
-            my $value = $alias->{$key};
-            puts "$key: $value";
+            if ( exists $alias->{$key} ) {
+                my $value = $alias->{$key};
+                $value = '' unless defined $value;
+                puts "$key: $value";
+            }
+            else {
+                puts "$key: <not exist>";
+            }
         }
     }
     else {
@@ -83,6 +101,16 @@ __END__
 =head1 NAME
 
 Beagle::Cmd::Command::alias - manage aliases
+
+=head1 SYNOPSIS
+
+    $ beagle alias # show current aliases
+    $ beagle aliases # ditto
+    $ beagle alias articles # show alias articles
+    $ beagle alias today yesterday # show aliases today and yesterday
+
+    $ beagle alias --set 'homer=ls homer' --set 'bart=ls bart'
+    $ beagle alias --unset homer --unset bart
 
 =head1 AUTHOR
 
