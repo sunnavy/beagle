3f86194b19bab98aee6ea4fb8b1aebf3536aeec4
sunnavy <sunnavy@gmail.com>
2011-07-26 16:26:51 +0800

    support -a for info cmd

--- lib/Beagle/Cmd/Command/info.pm
+++ lib/Beagle/Cmd/Command/info.pm
@@ -41,63 +41,36 @@ has 'message' => (
     traits        => ['Getopt'],
 );
 
+has 'all' => (
+    isa           => 'Bool',
+    is            => 'rw',
+    documentation => 'all the beagles',
+    cmd_aliases   => 'a',
+    traits        => ['Getopt'],
+);
+
 no Any::Moose;
 __PACKAGE__->meta->make_immutable;
 
 sub execute {
     my ( $self, $opt, $args ) = @_;
 
-    my $root = current_root('not die');
-
-    if ( !$root ) {
-        die "please specify beagle by --name or --root";
+    my @bh;
+    if ( $self->all ) {
+        @bh = values %{handles()};
+    }
+    else {
+        @bh = current_handle or die "please specify beagle by --name or --root";
     }
 
-    require Beagle::Handle;
-    my $bh = Beagle::Handle->new( root => $root );
-    my $info = $bh->info;
+    my $first = 1;
+    for my $bh (@bh) {
+        puts '=' x term_width() unless $first;
+        undef $first if $first;
 
-    my $template = encode_utf8 $info->serialize(
-        $self->verbose
-        ? (
-            created => 1,
-            updated => 1,
-            id      => 1,
-          )
-        : (
-            created => undef,
-            updated => undef,
-            id      => undef,
-        )
-    );
+        my $info = $bh->info;
 
-    if ( $self->edit || $self->set || $self->unset ) {
-
-        if ( $self->unset ) {
-
-            for my $key ( @{ $self->unset } ) {
-                if ( $info->can($key) ) {
-                    $info->$key('');
-                }
-                else {
-                    warn "unknown key: $key";
-                }
-            }
-        }
-
-        if ( $self->set ) {
-            for my $item ( @{ $self->set } ) {
-                my ( $key, $value ) = split /=/, $item, 2;
-                if ( $info->can($key) ) {
-                    $info->$key($value);
-                }
-                else {
-                    warn "unknown key: $key";
-                }
-            }
-        }
-
-        $template = encode_utf8 $info->serialize(
+        my $template = encode_utf8 $info->serialize(
             $self->verbose
             ? (
                 created => 1,
@@ -111,32 +84,73 @@ sub execute {
             )
         );
 
-        if ( $self->edit ) {
+        if ( $self->edit || $self->set || $self->unset ) {
 
-            my $updated = edit_text($template);
+            if ( $self->unset ) {
 
-            if ( !$self->force && $template eq $updated ) {
-                puts "aborted.";
-                return;
+                for my $key ( @{ $self->unset } ) {
+                    if ( $info->can($key) ) {
+                        $info->$key('');
+                    }
+                    else {
+                        warn "unknown key: $key";
+                    }
+                }
             }
 
-            $info = $info->new_from_string( decode_utf8 $updated);
-        }
+            if ( $self->set ) {
+                for my $item ( @{ $self->set } ) {
+                    my ( $key, $value ) = split /=/, $item, 2;
+                    if ( $info->can($key) ) {
+                        $info->$key($value);
+                    }
+                    else {
+                        warn "unknown key: $key";
+                    }
+                }
+            }
 
-        if (
-            $bh->update_info(
-                $info, message => $self->message || "updated info"
-            )
-          )
-        {
-            puts "updated info.";
+            $template = encode_utf8 $info->serialize(
+                $self->verbose
+                ? (
+                    created => 1,
+                    updated => 1,
+                    id      => 1,
+                  )
+                : (
+                    created => undef,
+                    updated => undef,
+                    id      => undef,
+                )
+            );
+
+            if ( $self->edit ) {
+
+                my $updated = edit_text($template);
+
+                if ( !$self->force && $template eq $updated ) {
+                    puts "aborted.";
+                    return;
+                }
+
+                $info = $info->new_from_string( decode_utf8 $updated);
+            }
+
+            if (
+                $bh->update_info(
+                    $info, message => $self->message || "updated info"
+                )
+              )
+            {
+                puts "updated info.";
+            }
+            else {
+                die "failed to update info.";
+            }
         }
         else {
-            die "failed to update info.";
+            puts decode_utf8 $template;
         }
-    }
-    else {
-        puts decode_utf8 $template;
     }
 }
 
